fb21e82aff1a4e7af42dedaf10fd00dd7dabcdc2
Joris Grilleres <jorisgrilleres@gmail.com>
2022-06-02 09:17:50 +0200

    Modif pull request

--- controllers/connexion.php
+++ controllers/connexion.php
@@ -1,18 +1,16 @@
 <?php
+    session_start();
 
-session_start();
-
-include(__DIR__."/../config.php"); 
-if(isset($_POST['login'])){
+    include(__DIR__."/../config.php"); 
     if (!empty($_POST['name']) && !empty($_POST['password'])) {
         $name = $_POST['name'];
         $password = $_POST['password'];
-        $getUser = "SELECT * FROM users WHERE name = '$name'";
-        $getUserStatement = $login->prepare($getUser);
+        $getUserQuery = "SELECT * FROM users WHERE name = '$name'";
+        $getUserStatement = $login->prepare($getUserQuery);
         $getUserStatement->execute();
-        $getUser = $getUserStatement->fetchAll();
+        $users = $getUserStatement->fetchAll();
         if (count($getUser) == 1) {
-            $user = $getUser[0];
+            $user = $users[0];
             if (password_verify($password, $user['password'])) {
                 $_SESSION['name'] = $name;
                 $_SESSION['auth'] = 1;
@@ -29,5 +27,4 @@ if(isset($_POST['login'])){
     else {
         header("location: ../pages/login.php?error=2");
     }
-}
 ?>
